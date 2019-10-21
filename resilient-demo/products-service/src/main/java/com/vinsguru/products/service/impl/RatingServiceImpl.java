83bea893b56a171b8a4b30889ec57c70f3e33c60
vins <vino.infy@gmail.com>
2019-10-21 00:21:49 -0500

    retry-pattern

--- resilient-demo/products-service/src/main/java/com/vinsguru/products/service/impl/RatingServiceImpl.java
+++ resilient-demo/products-service/src/main/java/com/vinsguru/products/service/impl/RatingServiceImpl.java
@@ -4,8 +4,13 @@ import com.vinsguru.dto.ProductRatingDTO;
 import com.vinsguru.products.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.SocketTimeoutException;
 
 @Service
 public class RatingServiceImpl implements RatingService {
@@ -17,15 +22,19 @@ public class RatingServiceImpl implements RatingService {
     private RestTemplate restTemplate;
 
     @Override
+    @Retryable(
+            value = {SocketTimeoutException.class},
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 1000)
+    )
     public ProductRatingDTO getRatings(int productId) {
         String url = this.ratingServiceUrl + "/" + productId;
-        ProductRatingDTO productRatingDTO = new ProductRatingDTO();
-        try{
-            productRatingDTO = this.restTemplate.getForObject(url, ProductRatingDTO.class);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return productRatingDTO;
+        return this.restTemplate.getForObject(url, ProductRatingDTO.class);
+    }
+
+    @Recover
+    public ProductRatingDTO getRecoveryRatings() {
+        return new ProductRatingDTO();
     }
 
 }
